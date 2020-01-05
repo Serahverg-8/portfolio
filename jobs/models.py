cfce563e6fdf87e4bf6c372f92032c62187f6a9b
Serahverg-8 <serahverg123@gmail.com>
2020-01-05 12:14:35 +0530

    jobs done

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    #url images/  first goes to media folder and then images in media because of specifying media root in the settings
+    image=models.ImageField(upload_to='images/')
+    #requires pillow so install pillow
+    summary=models.CharField(max_length=200)
